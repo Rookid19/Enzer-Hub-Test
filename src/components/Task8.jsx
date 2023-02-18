c6be4e0d432e4159b73328658450e213f5531eba
rookid19 <randyodoom19@gmail.com>
2023-02-18 15:02:11 +0000

    task 8

--- src/components/Task8.jsx
+++ src/components/Task8.jsx
@@ -1,31 +1,63 @@
 import axios from "axios";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Task8() {
+  const [data, setData] = useState([]);
+
   const fetchData = async () => {
     await axios
       .get("https://jsonplaceholder.typicode.com/users")
-      .then((res) => console.log(res.data));
+      .then((res) => {
+        setData(res.data);
+        console.log(res.data);
+      });
   };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
       <table>
-        <tr>
-          <th>id</th>
-          <th>name</th>
-          <th>username</th>
-          <th>street</th>
-          <th>suite</th>
-          <th>city</th>
-          <th>zip code</th>
-          <th>lat</th>
-          <th>lng</th>
-        </tr>
-        <tr>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td>Germany</td>
-        </tr>
+        <tbody>
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>username</th>
+            <th>street</th>
+            <th>suite</th>
+            <th>city</th>
+            <th>zip code</th>
+            <th>lat</th>
+            <th>lng</th>
+            <th>phone</th>
+            <th>website</th>
+            <th>company name</th>
+            <th>catch phrase</th>
+            <th>bs</th>
+
+
+          </tr>
+          {data.map(({ id ,name,username,address,phone,website,company}) => (
+            <tr key={id}>
+              <td>{id}</td>
+              <td>{name}</td>
+              <td>{username}</td>
+              <td>{address.street}</td>
+              <td>{address.suite}</td>
+              <td>{address.city}</td>
+              <td>{address.zipcode}</td>
+              <td>{address.geo.lat}</td>
+              <td>{address.geo.lng}</td>
+              <th>{phone}</th>
+            <th><a href={`http://${website}`}>{website}</a> </th>
+            <th>{company.name}</th>
+            <th>{company.catchPhrase}</th>
+            <th>{company.bs}</th>
+            </tr>
+          ))}
+        </tbody>
       </table>
       <br />
       <br />
