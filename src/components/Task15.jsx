8d0df54f84788a609db616cf98966e3bce0c707f
rookid19 <randyodoom19@gmail.com>
2023-02-21 17:13:56 +0800

    task15

--- src/components/Task15.jsx
+++ src/components/Task15.jsx
@@ -3,29 +3,38 @@ import React from "react";
 function Task15() {
   const userSelection = [
     [2, 1, 5, 6, 7],
-    [1, 7, 3, 45],
-    [6, 2, 4, 5, 6],
+    [1, 7, 3, 4, 5],
+    [6, 2, 4, 5, 1],
+    [1, 5, 4, 2, 6],
+    [1, 5, 2, 7, 6]
   ];
   const machineDrawNumber = [1, 5, 2, 7, 6];
 
+  const match = () => {
+    // We use the every() method to check if each number in the
+    //  row matches the machine draw number at the same position.
+    const matchedRows = userSelection.filter((row) => {
+      return row.every((number, j) => number === machineDrawNumber[j]);
+    });
 
-const match = () =>{
-
-    userSelection.forEach((row, i) => {
-        row.forEach((number, j) => {
-          if (number === machineDrawNumber[j]) {
-            console.log(`User has a match at position ${j+1} in row ${i+1}`);
-          } else {
-            // console.log(`User does not have a match at position ${j+1} in row ${i+1}`);
-          }
-        });
-      });
-}
+    console.log(matchedRows);
+    if (matchedRows.length > 0) {
+      console.log(
+        `The following rows have all numbers matching the machine draw numbers: ${matchedRows
+          .map((row, i) => i + 1)
+          .join(", ")}`
+      );
+    } else {
+      console.log("No row has all numbers matching the machine draw numbers.");
+    }
+  };
 
   return (
     <div>
       <h1>Task15</h1>
-      <button id="submit1" onClick={match}>Show match</button>
+      <button id="submit1" onClick={match}>
+        Show match
+      </button>
     </div>
   );
 }
