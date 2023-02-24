81c1d27d65e9c89da9e3a8576a964220fd68596e
rookid19 <randyodoom19@gmail.com>
2023-02-24 11:52:11 +0800

    timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -20,6 +20,16 @@ function Timer() {
     return randomNumber.padStart(5, "0");
   }
 
+  /**
+   * The useEffect hook takes a function as its first argument. This function is called when the component mounts,
+   * and then again whenever any of the values in the second argument (an array of dependencies) change.
+   * In this case, the function creates a new interval using the setInterval function. This interval runs every 1000 milliseconds (1 second)
+   * and calls the setTimeRemaining function, passing in a new value calculated by subtracting 1 from the previous value of timeRemaining.
+   * The if statement checks if timeRemaining is equal to 0. If it is, it clears the interval using the clearInterval function.
+   *  This is important because otherwise the interval would keep running even after the timer has reached 0.
+   * @return {*}
+   */
+
   useEffect(() => {
     const intervalId = setInterval(() => {
       setTimeRemaining((prevTime) => prevTime - 1);
@@ -32,19 +42,43 @@ function Timer() {
     return () => clearInterval(intervalId);
   }, [timeRemaining]);
 
+  /**
+   * The first line of the function sets the timeRemaining value in localStorage to the current value of timeRemaining.
+   * This allows the timer to persist across page refreshes or component unmounts.
+   * The if statement checks if timeRemaining is equal to 0. If it is, it means the timer has reached its end, and so the code resets
+   * the timeRemaining to 10 seconds and generates a new random number using the generateRandomNumber function.
+   * The new random number is also saved in localStorage and set in the component's state using the setRandomNumber function.
+   * By default, useEffect only runs after the first render, but by including timeRemaining in the array of dependencies,
+   * we can also trigger the function whenever timeRemaining changes.
+   *
+   * @return {*}
+   */
+
   useEffect(() => {
     localStorage.setItem("timeRemaining", timeRemaining);
 
     if (timeRemaining === 0) {
       setTimeRemaining(10);
-      let random_number = generateRandomNumber()
-    localStorage.setItem("randomNumber", random_number);
-
+      let random_number = generateRandomNumber();
+      localStorage.setItem("randomNumber", random_number);
       setRandomNumber(random_number);
     }
   }, [timeRemaining]);
 
-  // Convert seconds to "00:00:00" format
+  /**
+   * The function first calculates the number of hours in the input time value by dividing it by 3600 (the number of seconds in an hour)
+   * and taking the floor of the result using Math.floor(). The result is then converted to a string and padded with a leading zero using
+   * String.prototype.padStart() to ensure that it has two digits.
+   * The function then calculates the number of minutes in the remaining time by taking the modulus of time with 3600
+   * (to get the number of seconds remaining after subtracting the hours) and dividing by 60 (the number of seconds in a minute).
+   * The result is similarly converted to a string and padded with a leading zero.
+   * Finally, the function calculates the number of seconds remaining by taking the modulus of time with 60 and converting it to a string
+   * padded with a leading zero.
+   * All three values are concatenated together into a string in the format "hh:mm:ss" using template literals and returned from the function.
+   *
+   * @param {*} time
+   * @return {*}
+   */
   const formatTime = (time) => {
     const hours = Math.floor(time / 3600)
       .toString()
@@ -57,9 +91,9 @@ function Timer() {
   };
 
   return (
-    <div>
-      {formatTime(timeRemaining)}
-      <h3>{randomNumber}</h3>
+    <div className="timer">
+      <h3 className="time_remaining">{formatTime(timeRemaining)}</h3>
+      <h3 className="random_number">{randomNumber}</h3>
     </div>
   );
 }
