08f846d20e22487ac5016497d08f7f2ca3f0d14b
rookid19 <randyodoom19@gmail.com>
2023-02-24 15:03:30 +0800

    api

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -6,16 +6,20 @@ function Api() {
 
 
 
-  const fetch5d = async () => {
+  const fetchData = async (id) => {
     await axios
-      .get(`${host}/5d`)
+      .get(`${host}/${id}`)
       .then((res) => console.log(res.data))
       .catch((e) => alert(e));
   };
 
   return (
     <div>
-      <button onClick={fetch5d}>5d</button>
+      <button onClick={() => fetchData("5d")}>5d</button>
+      <button onClick={() => fetchData("3d")}>3d</button>
+      <button onClick={() => fetchData("fast4")}>5d</button>
+      <button onClick={() => fetchData("double")}>5d</button>
+
     </div>
   );
 }
